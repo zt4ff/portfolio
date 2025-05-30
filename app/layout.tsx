c1f0dec96c120425a137bc81154cf76135a79d92
Kayode Oluwasegun <segunkayode00@gmail.com>
2025-05-30 01:41:19 +0100

    feat: add web analytics to my page

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,6 +3,7 @@ import "@/app/globals.css";
 // import { Inter } from "next/font/google"
 import { ThemeProvider } from "@/components/theme-provider";
 import Navbar from "@/components/navbar";
+import { Analytics } from "@vercel/analytics/next";
 
 // const inter = Inter({ subsets: ["latin"] })
 
@@ -28,6 +29,7 @@ export default function RootLayout({
         >
           <Navbar />
           {children}
+          <Analytics />
         </ThemeProvider>
       </body>
     </html>
